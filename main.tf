9362f773db02b800ac74430ed167440607711e0b
ankit26290 <ankit26290@gmail.com>
2020-04-14 21:18:33 -0400

    commit

--- main.tf
+++ main.tf
@@ -1,15 +1,15 @@
 # Terraform state will be stored in S3
 terraform {
   backend "s3" {
-    bucket = "terraform-bucket-alex"
+    bucket = "terraform-jenkins-ankit"
     key    = "terraform.tfstate"
-    region = "us-east-1"
+    region = "ca-central-1"
   }
 }
 
 # Use AWS Terraform provider
 provider "aws" {
-  region = "us-east-1"
+  region = "ca-central-1"
 }
 
 # Create EC2 instance
